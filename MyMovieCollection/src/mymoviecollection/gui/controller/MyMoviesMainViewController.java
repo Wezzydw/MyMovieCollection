5297864c1f81d117d193be675c5a3411e29541b2
andr4229 <andr4229@hotmail.com>
2019-01-08 10:02:40 +0100

    knapper

--- MyMovieCollection/src/mymoviecollection/gui/controller/MyMoviesMainViewController.java
+++ MyMovieCollection/src/mymoviecollection/gui/controller/MyMoviesMainViewController.java
@@ -7,6 +7,8 @@ package mymoviecollection.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -25,5 +27,40 @@ public class MyMoviesMainViewController implements Initializable
     {
         // TODO
     }    
+
+    @FXML
+    private void btnRemoveCat(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void btnAddCat(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void btnRemoveMov(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void btnAddMov(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void btnEditMov(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void btnRenameCat(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void btnRate(ActionEvent event)
+    {
+    }
     
 }
