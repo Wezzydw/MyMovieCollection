4556d3b2243415f0e09c8834464bfc2b4f16da66
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2019-01-08 11:12:34 +0100

    Revert "."
    This reverts commit 289e16dcb44d55726a25350be0525fd326158670.

--- MyMovieCollection/src/mymoviecollection/dal/DatabaseConnection.java
+++ MyMovieCollection/src/mymoviecollection/dal/DatabaseConnection.java
@@ -23,15 +23,16 @@ public class DatabaseConnection {
 
     public DatabaseConnection() throws IOException {
         Properties databaseProperties = new Properties();
-        databaseProperties.load(new FileInputStream(PROP_FILE));
+         databaseProperties.load(new FileInputStream(PROP_FILE));
         ds = new SQLServerDataSource();
         ds.setServerName(databaseProperties.getProperty("servername"));
         ds.setDatabaseName(databaseProperties.getProperty("databasename"));
         ds.setUser(databaseProperties.getProperty("user"));
         ds.setPassword(databaseProperties.getProperty("pw"));
     }
-
-    public Connection getConnection() throws SQLServerException {
+    
+        public Connection getConnection() throws SQLServerException
+    {
         return ds.getConnection();
     }
 }
