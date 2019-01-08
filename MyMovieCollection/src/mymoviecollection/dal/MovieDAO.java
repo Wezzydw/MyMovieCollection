fee7c1fcba2becda9b67c302e780eecac12dbc3e
Warfor <44639824+Warfor@users.noreply.github.com>
2019-01-08 11:50:21 +0100

    MovieDAO deleteMovies

--- MyMovieCollection/src/mymoviecollection/dal/MovieDAO.java
+++ MyMovieCollection/src/mymoviecollection/dal/MovieDAO.java
@@ -5,10 +5,44 @@
  */
 package mymoviecollection.dal;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import mymoviecollection.be.Movie;
+
 /**
  *
  * @author Andreas Svendsen
  */
 public class MovieDAO {
+       DatabaseConnection conProvider;
+        List<Movie> movies;
+
     
+    public MovieDAO() throws IOException{
+    conProvider = new DatabaseConnection();
+    movies = new ArrayList();
+    
+    }
+        public void deleteMovies(List<Movie> selectedMovie) throws IOException
+    {
+        try (Connection con = conProvider.getConnection())
+        {
+            String a = "DELETE FROM Movies WHERE Id =?;";
+            PreparedStatement prst = con.prepareStatement(a);
+            for (Movie movie : movies)
+            {
+                prst.setInt(1, movie.getId());
+                prst.addBatch();
+            }
+            prst.executeBatch();
+
+        } catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
 }
