63a9f8d85870dc5de052ee43021c93c415cd2231
Warfor <44639824+Warfor@users.noreply.github.com>
2019-01-11 11:13:42 +0100

    så kan der ses film

--- MyMovieCollection/src/mymoviecollection/bll/Player.java
+++ MyMovieCollection/src/mymoviecollection/bll/Player.java
@@ -14,11 +14,15 @@ import java.io.IOException;
 
 public class Player {
   /**
-   * Mangler at blive tilkoblet knap
+   * Denne metode åbner VLC playeren igennem programmet, så der herved kan afspilles film ved brug af VLC.
    */
     public void callVlc(String filePath){
+        String qq = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe";
+        String test = "\"" + qq + "\"";
+        String l = "\\\\wezzy\\Film\\Avengers.Infinity.War.2018.NORDiC.REMUX.1080p.BluRay.AVC.DTS-HD.MA.5.1-CDB\\Avengers.Infinity.War.2018.NORDiC.REMUX.1080p.BluRay.AVC.DTS-HD.MA.5.1-CDB.mkv";
                try {
-        Process p = Runtime.getRuntime().exec("\"C:\\Program Files\\VideoLAN\\VLC\\vlc.exe " + filePath);
+        Process p = Runtime.getRuntime().exec(test + filePath);
+                   System.out.println(filePath);
     } catch (IOException e){
         e.printStackTrace();
     }
