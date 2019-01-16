a4916c2c5544908e21349e51ce15c95b49a4ed65
mapoul <mpoulsen1989@gmail.com>
2019-01-16 10:07:37 +0100

    javadokumentar

--- MyMovieCollection/src/mymoviecollection/bll/Search.java
+++ MyMovieCollection/src/mymoviecollection/bll/Search.java
@@ -21,10 +21,16 @@ public class Search {
     private List<Movie> movie;
     
     public Search(){
-       // mdao = new MovieDAO();
-       // movie = manager.getAllMovies();
+       
     }
-
+    
+    /**
+     * Denne metode looper gennem input fra brugeren, og ser om film-titlerne
+     * indholder søgeordet query.
+     * @param query
+     * @param movie
+     * @return en liste af film der indholder søgeordet.
+     */
     public List<Movie> searchMovie(String query, List<Movie> movie) {
         List<Movie> searchResult = new ArrayList();
         
@@ -42,51 +48,49 @@ public class Search {
         return searchResult;
     }
     
-    public List<Movie> sortCategories(List<Boolean> checkList, List<Movie> movies, List<Category> categories){
-        
+    /**
+     * Denne metode sorterer listen film, efter hvilke kategorier der er tjekket
+     * af i menuboxen. Den tjekker også kategorierne for om de er tjekket af
+     * eller ej.
+     * @param checkList
+     * @param movies
+     * @param categories
+     * @return sortResult.
+     */
+    public List<Movie> sortCategories(List<Boolean> checkList, List<Movie> movies, List<Category> categories) {
+
         List<Movie> sortResult = new ArrayList();
         List<Category> checkTrue = new ArrayList();
         int counter = 0;
-        if (checkList == null)
-        {
+        if (checkList == null) {
             return movies;
         }
         for (Boolean boolean1 : checkList) {
-            if(boolean1){
+            if (boolean1) {
                 checkTrue.add(categories.get(counter));
             }
             counter++;
         }
         System.out.println(checkTrue.size());
-        if (checkTrue.size()==0)
+        if (checkTrue.size() == 0) {
             return movies;
-        System.out.println("Grimme pige" + movies.size());
+        }
         for (Movie movy : movies) {
-            //hver enesete film
             int c1 = 0;
             for (String category : movy.getCategory()) {
-                //en liste af deres kategorier
-                
-                for (Category bæh: checkTrue){
-                    //checkede kategorier fra menuButton
-                    System.out.println(movy.getTitle()+ bæh + category);
+                for (Category bæh : checkTrue) {
+                    System.out.println(movy.getTitle() + bæh + category);
                     int c2 = 0;
-                    if(category.equals(bæh.toString())){
-                        System.out.println(movy.getTitle()+ bæh+ " sådan noget");
+                    if (category.equals(bæh.toString())) {
                         c1++;
-                        if(c1==checkTrue.size())
-                        {
-                            
-                            sortResult.add(movy);   
+                        if (c1 == checkTrue.size()) {
+                            sortResult.add(movy);
                         }
                     }
-
                 }
-            }    
-            
             }
+        }
         return sortResult;
     }
-    
-    
+
 }
